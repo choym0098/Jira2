d114cc6b9ed81f340b3652595121e3875e7d0dca
Julie Lee <43656143+julielee9067@users.noreply.github.com>
2020-09-09 22:18:52 +0900

    Add column table (#1)
    * fea) Reformat projects.py
    
    * fea) Add column table
    
    * fea) Apply pascal case
    
    * fea) Migrate to apply pascal case
    
    Co-authored-by: Julie Lee <julielee9067@gmail.com>

--- jira2app/models/projects.py
+++ jira2app/models/projects.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
-from django.db import models
 import uuid
 
-class Projects(models.Model):
-    projectId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    projectName = models.CharField(max_length=32, null=False)
-    projectDescription = models.TextField()
-    isPrivate = models.BooleanField(default=True, null=False)
-    createdTime = models.DateTimeField(auto_now_add=True, null=False)
-    modifiedTime = models.DateTimeField(auto_now=True, null=False)
+from django.db import models
 
-    # TODO: add foregin key after creating Users table
-    # userId = models.
+
+class Projects(models.Model):
+    project_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    project_name = models.CharField(max_length=32, null=False)
+    project_description = models.TextField()
+    is_private = models.BooleanField(default=True, null=False)
+    created_time = models.DateTimeField(auto_now_add=True, null=False)
+    modified_time = models.DateTimeField(auto_now=True, null=False)
+
+    # TODO: add foreign key after creating Users table
+    # user_id = models.
 
     class Meta:
-        app_label="jira2app"
-
+        app_label = "jira2app"
