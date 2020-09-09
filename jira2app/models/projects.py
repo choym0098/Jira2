192440c4ea53b6876dc3e16310762367b126e30e
Julie Lee <julielee9067@gmail.com>
2020-09-09 21:45:38 +0900

    fea) Apply pascal case

--- jira2app/models/projects.py
+++ jira2app/models/projects.py
@@ -5,15 +5,15 @@ from django.db import models
 
 
 class Projects(models.Model):
-    projectId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    projectName = models.CharField(max_length=32, null=False)
-    projectDescription = models.TextField()
-    isPrivate = models.BooleanField(default=True, null=False)
-    createdTime = models.DateTimeField(auto_now_add=True, null=False)
-    modifiedTime = models.DateTimeField(auto_now=True, null=False)
+    project_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    project_name = models.CharField(max_length=32, null=False)
+    project_description = models.TextField()
+    is_private = models.BooleanField(default=True, null=False)
+    created_time = models.DateTimeField(auto_now_add=True, null=False)
+    modified_time = models.DateTimeField(auto_now=True, null=False)
 
-    # TODO: add foregin key after creating Users table
-    # userId = models.
+    # TODO: add foreign key after creating Users table
+    # user_id = models.
 
     class Meta:
         app_label = "jira2app"
