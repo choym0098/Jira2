a6ccba63bca8625607790bc0e728353861c435e7
Julie Lee <julielee9067@gmail.com>
2020-09-11 08:43:13 +0900

    fea) Add comments table

--- jira2app/models/__init__.py
+++ jira2app/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .columns import Columns
+from .comments import Comments
 from .projects import Projects
 
-__all__ = ["Columns", "Projects"]
+__all__ = ["Columns", "Projects", "Comments"]
---
+++ jira2app/models/comments.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import uuid
+
+from django.db import models
+
+
+class Comments(models.Model):
+    comment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    ticket = models.ForeignKey("Tickets", on_delete=models.CASCADE)
+    user = models.ForeignKey("Users", on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now=True, null=False)
+    modified_time = models.DateTimeField(auto_now_add=True, null=False)
+    message = models.TextField(null=False, editable=True)
+
+    class Meta:
+        app_label = "jira2app"
