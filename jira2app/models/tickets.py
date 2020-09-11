4f99fa9a2689fa0230913843282b34c175fa863c
Julie Lee <julielee9067@gmail.com>
2020-09-11 19:09:16 +0900

    fea) Apply default Django User table as foreign key

--- jira2app/models/tickets.py
+++ jira2app/models/tickets.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import uuid
 
+from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
@@ -10,7 +11,7 @@ from jira2app.models import Columns
 class Tickets(models.Model):
     ticket_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     column = models.ForeignKey(Columns, on_delete=models.CASCADE)
-    user = models.ForeignKey("Users", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now=True, null=False)
     modified_time = models.DateTimeField(auto_now_add=True, null=False)
     ticket_description = models.TextField(null=True, editable=True)
