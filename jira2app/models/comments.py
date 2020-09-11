825afda9a75c2a2e5b655ceb7844e776a56b384b
Julie Lee <julielee9067@gmail.com>
2020-09-11 18:54:29 +0900

    fea) Fix foreign key object for tickets in comments table

--- jira2app/models/comments.py
+++ jira2app/models/comments.py
@@ -3,10 +3,12 @@ import uuid
 
 from django.db import models
 
+from jira2app.models import Tickets
+
 
 class Comments(models.Model):
     comment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    ticket = models.ForeignKey("Tickets", on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Tickets, on_delete=models.CASCADE)
     user = models.ForeignKey("Users", on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now=True, null=False)
     modified_time = models.DateTimeField(auto_now_add=True, null=False)
