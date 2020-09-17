8a45f29a10b0ff361dde9ff2c2101cd2da0fa65a
Julie Lee <julielee9067@gmail.com>
2020-09-17 23:59:57 +0900

    fea) Add default sign up files

--- jira2app/views.py
+++ jira2app/views.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth import login as user_login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def sign_up(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user_login(request, user)
+        return redirect("jira2app:sign_up")
+    else:
+        form = UserCreationForm()
+        return render(request, "jira2app/form.html", {"form": form})
