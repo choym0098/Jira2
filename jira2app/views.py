6d7baa7fe42e8fb2b6875fda276ce7dc06089f63
Julie Lee <43656143+julielee9067@users.noreply.github.com>
2020-09-22 16:51:24 +0900

    Add Sign Up API (#5)
    * fea) Add default sign up files
    
    * fea) Integrate db with signup fields
    
    Co-authored-by: Julie Lee <julielee9067@gmail.com>

--- jira2app/views.py
+++ jira2app/views.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST["password"] == request.POST["password_verification"]:
+            user = User.objects.create_user(
+                username=request.POST["username"],
+                email=request.POST["email"],
+                first_name=request.POST["first_name"],
+                last_name=request.POST["last_name"],
+                password=request.POST["password"],
+            )
+            auth.login(request, user)
+            return redirect("home")
+    return render(request, "jira2app/signup.html")
