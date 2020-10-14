6a3e54904d3c7edcdea7319106bfe0e2b7b3920e
choym0098 <choym0098@gmail.com>
2020-10-14 07:28:11 -0400

    Added comment model/api

--- app/jira2app/admin.py
+++ app/jira2app/admin.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Column, Project, UserProfile, Ticket
+from .models import Column, Project, UserProfile, Ticket, Comment
 
 admin.site.register(UserProfile)
 admin.site.register(Project)
 admin.site.register(Column)
 admin.site.register(Ticket)
+admin.site.register(Comment)
--- app/project/views.py
+++ app/project/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from jira2app.models import Project
+from jira2app.models import Project, Ticket
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, TicketDetailSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -14,3 +14,12 @@ class ProjectViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     serializer_class = ProjectSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    """Manage tickets in the database"""
+
+    queryset = Ticket.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = TicketDetailSerializer
